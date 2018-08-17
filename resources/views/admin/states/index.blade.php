63aaeaa7057da8637d3e669ce40b387b30dfc8b8
Leandro Leonel Rocha <llrocha@sds_domain_1.local>
2018-08-17 10:39:06 -0300

    orden estado iniciado

--- resources/views/admin/states/index.blade.php
+++ resources/views/admin/states/index.blade.php
@@ -5,7 +5,7 @@
                 <td style="width: 1%"><input class="id_destroy" value="{{$model->id}}" type="checkbox"></td>
                 <td>{{$model->id}}</td>
                 <td>{{$model->description }}</td>
-                <td>{{$model->text_email}}</td>  
+                <td>{!! $model->text_email !!}</td>  
                 <td><div style="widht:5px; height:20px; background-color:{{$model->color}};" ></div> </td>   
                 <td></td>           
             </tr>
