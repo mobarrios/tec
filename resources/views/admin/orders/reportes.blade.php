3c3e68fbc3d03ee0f7664537d8941d468a658a7d
Leandro Leonel Rocha <llrocha@sds_domain_1.local>
2018-09-05 10:45:49 -0300

    update view email

--- resources/views/admin/orders/reportes.blade.php
+++ resources/views/admin/orders/reportes.blade.php
@@ -89,12 +89,14 @@
              <table style="width: 100%;">
                   <tr>
                       <td style="width: 50%;">
+                        @if($company->images->count() > 0)
                         <img width="200px" src="{{ $company->images->first()->path }}">
+                        @endif
                          
                       </td>
                       <td style="font-size: 1.5em;" >
                           <strong>Entrada</strong> {{ date('d/m/Y',strtotime($model->fecha_inicio)) }}<br />
-                          <strong>Orden de Reparacion</strong> ist-{{$model->id}}
+                          <strong>Orden de Reparacion</strong> {{$model->id}}
                       </td>
                   </tr>
               </table>
