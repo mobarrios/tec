6d5f5847e5f5d99bbd2e74ae826d2bbdd46b4cf3
Manuel Barrios <mbarrios@Manuels-MBP.fibertel.com.ar>
2020-05-01 09:14:44 -0300

    update

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
-    'version' => '200430',
+    'version' => '200501',
 
     'debug' => env('APP_DEBUG', false),
 
